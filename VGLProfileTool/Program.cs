2952b731cdfd56accca4fe60409bb7fbe2464438
Nicksin <nicksinbusiness@gmail.com>
2024-02-01 12:59:58 -0700

    2nd commit

---
+++ VGLProfileTool/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter background link: ");
+        string backgroundInput = Console.ReadLine();
+
+        Console.Write("Enter profile pic link: ");
+        string profilePicInput = Console.ReadLine();
+
+        Console.Write("Enter some profile text: ");
+        string profileTextInput = Console.ReadLine();
+        profileTextInput = profileTextInput.Replace(" ", @"%20");
+
+        var bgData = new WebClient().DownloadString("http://localhost:42281/customization/background_image/update?link=" + backgroundInput);
+        var picData = new WebClient().DownloadString("http://localhost:42281/home/profile_picture/update?link=" + profilePicInput);
+        var textData = new WebClient().DownloadString("http://localhost:42281/home/profile_text/update?text=" + profileTextInput);
+    }
+}
